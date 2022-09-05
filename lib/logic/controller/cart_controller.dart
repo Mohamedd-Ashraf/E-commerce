ff0d558bdede97bdf0b127a78eaa2b7453fd5b86
Mohamedd-Ashraf <110347603+Mohamedd-Ashraf@users.noreply.github.com>
2022-09-05 23:35:33 +0200

    cartScreenDone

--- lib/logic/controller/cart_controller.dart
+++ lib/logic/controller/cart_controller.dart
@@ -1,10 +1,33 @@
 import 'package:e_commerce/models/product_model.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'dart:convert';
+
+import '../../uitils/themes.dart';
 
 class CartController extends GetxController {
   var productsMap = {}.obs;
+//   // GetStorage box = GetStorage();
+// @override
+//  void onInit(){
+//   super.onInit();
 
-  void addProductToCart(Product product) {
+ 
+//     final  storedShoppings = box.read('productsMap');
+//     print(storedShoppings);
+//     Map vmap=json.decode(storedShoppings);
+//     print(vmap);
+
+//     // if (storedShoppings != null) {
+//     //   // productsMap.values.join(storedShoppings.toString());
+//     //   // productsMap.assignAll(storedShoppings);
+//     //   productsMap=storedShoppings;
+        
+//     // }
+//  }
+
+  void addProductToCart(Product product) async{
     if (productsMap.containsKey(product)) {
       productsMap[product] += 1;
       print(productsMap.value.toString());
@@ -12,13 +35,19 @@ class CartController extends GetxController {
       productsMap[product] = 1;
       print(productsMap.value.toString());
     }
+    // var paymentsAsMap = productsMap.map((payment) => payment.toJson()).toList();
+
+    // await box.write("productsMap", productsMap.toString());
+    // print(storedShoppings);
+
+
   }
 
-  void removeProductsfromCart(Product product) {
+  void reduceProductInCart(Product product) {
     if (productsMap.containsKey(product) && productsMap[product] == 1) {
       productsMap.removeWhere((key, value) => key == product);
     } else {
-      productsMap[product] = -1;
+      productsMap[product] -= 1;
     }
   }
 
@@ -27,7 +56,34 @@ class CartController extends GetxController {
   }
 
   void clearAllProductsCart() {
-    productsMap.clear();
+    Get.defaultDialog(
+      title: "Logout From App",
+      titleStyle: const TextStyle(
+        fontSize: 18,
+        color: Colors.black,
+        fontWeight: FontWeight.bold,
+      ),
+      middleText: 'Are you sure you need to logout',
+      middleTextStyle: const TextStyle(
+        fontSize: 18,
+        color: Colors.black,
+        fontWeight: FontWeight.bold,
+      ),
+      // backgroundColor: Colors.grey,
+      radius: 10,
+      textCancel: " No ",
+      cancelTextColor: Colors.black,
+      textConfirm: " YES ",
+      confirmTextColor: Colors.white,
+      onCancel: () {
+        // Get.back();
+      },
+      onConfirm: () {
+        productsMap.clear();
+        Get.back();
+      },
+      buttonColor: Get.isDarkMode ? pinkClr : mainColor,
+    );
   }
 
   get productTotalPrice {
@@ -40,5 +96,17 @@ class CartController extends GetxController {
         .toList()
         .reduce((value, element) => value + element)
         .toStringAsFixed(2);
+  }
+
+  String quantity() {
+    if (productsMap.isEmpty) {
+      return "0";
+    }
+
+    return productsMap.entries
+        .map((e) => e.value)
+        .toList()
+        .reduce((value, element) => element + value)
+        .toString();
   }
 }
