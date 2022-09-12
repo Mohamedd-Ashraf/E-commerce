b2f4b3abb4d9f52c86aac8c0e734f441ffea849d
Mohamedd-Ashraf <110347603+Mohamedd-Ashraf@users.noreply.github.com>
2022-09-12 20:47:43 +0200

    controllers edited

--- lib/logic/controller/product_cotroller.dart
+++ lib/logic/controller/product_cotroller.dart
@@ -13,11 +13,13 @@ class ProductController extends GetxController {
   var searchList = <Product>[].obs;
   TextEditingController searchTextController = TextEditingController();
 
+
+
   @override
  void onInit(){
   super.onInit();
   getProducts();
- 
+  
     List? storedShoppings = box.read<List>('FavouriteList');
 
     if (storedShoppings != null) {
